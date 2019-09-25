c67ee868e19a78dc78c75f37dcc5ddfff69177f8
Marius Vintilas <inspirenat@gmail.com>
2019-09-25 12:26:56 +0300

    stuff

--- Resources/Player.cs
+++ Resources/Player.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace KillAllNeighbors.Resources
 {
-    class Player
+    public class Player
     {
+        Vector2 currentCoord;
+        int coins;
+        int score;
+        float health;
+        int id;
+        string name;
+
+        public Player()
+        {
+            currentCoord = new Vector2();
+            coins = 0;
+            score = 0;
+            health = 100;
+            name = "AAAAAAAAAAA";
+        }
+
+        public void SetId(int _id)
+        {
+            id = _id;
+        }
     }
 }
